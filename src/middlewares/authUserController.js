aeebe2629eb31edfe3ca48f18adc91cc2e3241ad
Naxo <naxorubio@hotmail.com>
2023-12-17 10:16:31 +0100

    actualizado, comprobado y funcionando, añado selectalllinksmodel

--- src/middlewares/authUserController.js
+++ src/middlewares/authUserController.js
@@ -1,30 +1,33 @@
 import jwt from 'jsonwebtoken';
-import { invalidCredentialsError, notAuthenticatedError } from '../services/errorService.js';
+import {
+  invalidCredentialsError,
+  notAuthenticatedError,
+} from '../services/errorService.js';
 
+const authUserController = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
 
-const authUserController = (req,res,next) => { 
-    try {
-        const { authorization } = req.headers;
-
-        if(!authorization){
-            notAuthenticatedError();
-        }
-
-        let tokenInfo; //si viene el token 
-
-        try {
-            tokenInfo = jwt.verify(authorization, process.env.SECRET);
-        } catch (error) {
-            invalidCredentialsError();
-        }
-
-        req.user = tokenInfo;
-
-        next();
-    } catch (error) {
-        next(error);
+    if (!authorization) {
+      notAuthenticatedError();
     }
-}
 
+    let tokenInfo; //si viene el token
+
+    try {
+      tokenInfo = jwt.verify(authorization, process.env.SECRET);
+    } catch (error) {
+      invalidCredentialsError();
+    }
+
+    req.user = tokenInfo;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default authUserController;
+
+//este authuser lo implementamos también en los links
